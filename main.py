31b43038335f52fb3bdb1824b334846925585aa6
DenisLobas <denislobas.1620@icloud.com>
2025-12-06 17:37:33 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- main.py
+++
@@ -1,5 +0,0 @@
-
-def first_method(name):
-    print("Hello " + name)
-
-
---
+++ playwright_courses.py
@@ -0,0 +1,40 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    registration_email_input = page.get_by_test_id("registration-form-email-input").locator("input")
+    registration_email_input.fill("user.name@gmail.com")
+
+    registration_username_input = page.get_by_test_id("registration-form-username-input").locator("input")
+    registration_username_input.fill("username")
+
+    registration_password_input = page.get_by_test_id("registration-form-password-input").locator("input")
+    registration_password_input.fill("password")
+
+    registration_button = page.get_by_test_id("registration-page-registration-button")
+    registration_button.click()
+
+    context.storage_state(path="browser-state.json")
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    title_page_course = page.get_by_test_id("courses-list-toolbar-title-text")
+    expect(title_page_course).to_have_text("Courses")
+
+    text_no_results_page_course = page.get_by_test_id("courses-list-empty-view-title-text")
+    expect(text_no_results_page_course).to_have_text("There is no results")
+
+    icon_no_results_page_course = page.get_by_test_id("courses-list-empty-view-icon")
+    expect(icon_no_results_page_course).to_be_visible()
+
+    description_no_results_page_course = page.get_by_test_id("courses-list-empty-view-description-text")
+    expect(description_no_results_page_course).to_have_text("Results from the load test pipeline will be displayed here")
